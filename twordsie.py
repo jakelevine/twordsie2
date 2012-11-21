a6e1792d5f30a03df6c99ee3457c77516fad7463
jakelevine <levine.jake@gmail.com>
2012-11-21 14:57:31 -0500

    db setup, added links to mention and wordcount output

--- twordsie.py
+++ twordsie.py
@@ -45,14 +45,21 @@ def gettweets():
 	username_mentions = username+'_mentions'
 	username_wordcount = username+'_wordcount'
 	
-#	things = cache.get_many(username, username_mentions, username_wordcount)
-#	if things[0] is not None:
-#		return render_template("results.html",data=json.dumps(things[0]), mentions=things[1], wordcount=things[2])
+	things = cache.get_many(username, username_mentions, username_wordcount)
+	if things[0] is not None:
+		return render_template("results.html",data=json.dumps(things[0]), mentions=things[1], wordcount=things[2])
 		
 	url = 'http://api.twitter.com/1/statuses/user_timeline.json?screen_name='+username+'&count=200'
-	content = urllib2.urlopen(url)
+
+	while True:
+		try:
+			content = urllib2.urlopen(url)
+			break
+		except urllib2.HTTPError:
+			return "Please enter a valid Twitter handle. Unfortunately Twordsie does not support private accounts at this time."
+		
 	tweets = json.loads(content.read())
-	
+
 	data = {'cols': [{'type': 'string', 'label': 'Tweets'}],
 			'rows': [{'c': [{'v': tweet["text"]}]} for tweet in tweets]}
 	
@@ -64,7 +71,7 @@ def gettweets():
 	user = User(username=username, wordcount=wordcount, mentions=mentions, tweets = tweets)
 	user.save()
 	
-#	cache.set_many({username:data,username_mentions:mentions,username_wordcount:wordcount}, timeout=20 * 60)
+	cache.set_many({username:data,username_mentions:mentions,username_wordcount:wordcount}, timeout=20 * 60)
 	
 	return render_template("results.html",data=json.dumps(data), wordcount=wordcount, mentions=mentions)
 	
